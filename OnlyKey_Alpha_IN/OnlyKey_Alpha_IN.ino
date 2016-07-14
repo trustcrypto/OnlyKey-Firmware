6a13ea0345eaae7a534d1b76b5cb04fdd7550402
onlykey <root@localhost.localdomain>
2016-07-13 21:14:18 -0400

    Dev vm commit

--- OnlyKey_Alpha_IN/OnlyKey_Alpha_IN.ino
+++ OnlyKey_Alpha_IN/OnlyKey_Alpha_IN.ino
@@ -47,7 +47,7 @@
 /*************************************/
 //#define US_VERSION
 //Define for US Version Firmare
-//#define DEBUG
+#define DEBUG
 extern bool PDmode;
 #ifdef US_VERSION
 #include "yksim.h"
